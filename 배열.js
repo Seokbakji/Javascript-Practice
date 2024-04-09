8ca0b0eeef24819d328b169d27a364e5b011fd47
Seokbakji <75566043+Seokbakji@users.noreply.github.com>
2024-04-10 00:19:56 +0900

    자바스크립트 연습

--- 배열.js
+++ 배열.js
@@ -48,3 +48,65 @@ console.log(result);
 
 //map
 
+let newUnitList = unitList.map((unit, index) => {
+	return Object.assign({}, unit, {
+		id: index + 1,
+		isWeaponSword: unit.무기 === "검",
+	});
+});
+
+console.log(newUnitList);
+
+//split
+
+const str = "펠리시아 라피스 루플레 카무이 아쿠아"
+const strArr = str.split(" ");
+
+console.log(strArr);
+
+//Array.isArray
+
+const newUnit = Object.assign({}, unitList);
+
+console.log(typeof unitList); //배열
+console.log(typeof newUnit); //객체
+
+console.log(Array.isArray(unitList));
+console.log(Array.isArray(newUnit));
+
+//sort
+
+strArr.sort();
+console.log(strArr); //가나다 순으로 정렬
+
+let numbers = [27, 8, 5, 13];
+
+numbers.sort((i, j) => {
+	return i - j;
+
+	/*
+
+	27 - 8 > 0, 8 -> 27
+	8 - 5 > 0, 5 -> 8
+	5 - 13 < 0, 5 -> 13
+	13 - 27 < 0, 13 -> 27
+
+	5 8 13 27
+
+	*/
+
+});
+console.log(numbers);
+
+
+//arr.reduce((인자) => {함수}, [베열]);
+
+let swordUnit = unitList.reduce((prev, cur) => {
+
+	if (cur.무기 === "검") {
+		prev.push(cur.이름); //검이면 prev에 이름 추가
+	}
+	return prev; //swordUnit에 이름 반환
+}, []);
+
+console.log(swordUnit);
